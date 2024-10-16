5ca84b24a3bb3d670399f461c33ff198d141d1e5
Леонид Мартов <leonidmartov@MacBook-Air-Leonid.local>
2024-10-16 20:20:45 +0300

    ww

--- src/main/java/SpringApp/repositories/UsersRepo.java
+++
@@ -1,9 +0,0 @@
-package SpringApp.repositories;
-
-import SpringApp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepo extends JpaRepository<User, Integer> {
-}
